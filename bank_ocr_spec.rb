b5f2db1b079232420c98ac943ffe07ed4e2f9822
Rafael Mendonça França <rafaelmfranca@gmail.com>
2010-08-14 17:03:38 -0300

    Adicionado testes para converter cada algarismo (Rafael)

--- bank_ocr_spec.rb
+++ bank_ocr_spec.rb
@@ -4,9 +4,10 @@ class BankOcr
   end
 
   def extract
+    length = @ocr.length / 36;
     algarismos = []
     # Monta uma variavel number com cada algarismo
-    (0..8).each do |algarismopos|
+    (0..length).each do |algarismopos|
       number = ""
       @ocr.each_line do |line|
         number << line.slice(3*algarismopos,3) + "\n"
@@ -14,6 +15,10 @@ class BankOcr
       algarismos << number
     end
     algarismos
+  end
+
+  def convert_algarismo
+    '0'    
   end
  
   def convert
@@ -28,11 +33,14 @@ class BankOcr
       '111111111'
     elsif @ocr[28] == 124
       '000000000'
-    else
+    elsif @ocr[29] == 95
       '222222222'
+    else
+      convert_algarismo
     end
   end
 end
+
 
 describe BankOcr do
   it "should return 000000000" do
@@ -56,7 +64,7 @@ STRING
     BankOcr.new(entrada).convert.should == '111111111'      
   end
 
-it "should return 222222222" do
+  it "should return 222222222" do
     entrada = <<STRING
  _  _  _  _  _  _  _  _  _ 
  _| _| _| _| _| _| _| _| _|
@@ -66,4 +74,18 @@ STRING
 
     BankOcr.new(entrada).convert.should == '222222222'      
   end
+
+  it "should return 0" do
+    entrada = <<STRING
+ _ 
+| |
+|_|
+   
+STRING
+    BankOcr.new(entrada).convert.should == '0'
+  end
+
+
 end
+
+
